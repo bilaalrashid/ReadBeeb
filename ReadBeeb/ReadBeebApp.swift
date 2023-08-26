85fb248a84a3e120572828efee643f5da8a4a528
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-08-26 22:01:01 +0100

    Initial Commit

---
+++ ReadBeeb/ReadBeebApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadBeebApp.swift
+//  ReadBeeb
+//
+//  Created by Bilaal Rashid on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReadBeebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
