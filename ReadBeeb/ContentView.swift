5970fda6efdb7f415077ebff56c0f9bdc9c62e8b
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-08-26 22:56:17 +0100

    Add tab view

--- ReadBeeb/ContentView.swift
+++ ReadBeeb/ContentView.swift
@@ -9,13 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            NavigationStack {
+                Text("Hello, World!")
+                    .navigationTitle("Top Stories")
+            }
+            .navigationViewStyle(.stack)
+            .tabItem {
+                Label("Top Stories", systemImage: "doc.richtext")
+            }
+
+            NavigationStack {
+                Text("Hello, World!")
+                    .navigationTitle("My News")
+            }
+            .navigationViewStyle(.stack)
+            .tabItem {
+                Label("My News", systemImage: "person.crop.square")
+            }
+
+            NavigationStack {
+                Text("Hello, World!")
+                    .navigationTitle("Popular")
+            }
+            .navigationViewStyle(.stack)
+            .tabItem {
+                Label("Popular", systemImage: "chart.line.uptrend.xyaxis")
+            }
+
+            NavigationStack {
+                Text("Hello, World!")
+                    .navigationTitle("Video")
+            }
+            .navigationViewStyle(.stack)
+            .tabItem {
+                Label("Video", systemImage: "play.rectangle")
+            }
+
+            NavigationStack {
+                Text("Hello, World!")
+                    .navigationTitle("Search")
+            }
+            .navigationViewStyle(.stack)
+            .tabItem {
+                Label("Search", systemImage: "magnifyingglass")
+            }
         }
-        .padding()
     }
 }
 
