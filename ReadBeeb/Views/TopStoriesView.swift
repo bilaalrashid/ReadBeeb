ea56e06e79785f15bd4e83fbeb37bc4fbe90c14c
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-08-27 11:55:19 +0100

    Should error messages to the user on network error

--- ReadBeeb/Views/TopStoriesView.swift
+++ ReadBeeb/Views/TopStoriesView.swift
@@ -11,17 +11,25 @@ struct TopStoriesView: View {
 
     @State private var data: BBCNewsAPIHomeTabResult? = nil
 
+    @State private var shouldDisplayNetworkError = false
+
     var body: some View {
         List {
 
         }
+        .alert(
+            "Unable To Load Data",
+            isPresented: self.$shouldDisplayNetworkError,
+            actions: { Button("OK", role: .cancel) { } },
+            message: { Text("Please try again later and contact support if the problem persists") }
+        )
         .onAppear {
             Task {
                 do {
                     let result = try await BBCNewsAPINetworkController.fetchHomeTabData()
                     self.data = result
                 } catch let error {
-                    // Noop
+                    self.shouldDisplayNetworkError = true
                 }
             }
         }
