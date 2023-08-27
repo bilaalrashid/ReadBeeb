1896cb0303d315e8472e6190bda368bddcc434fb
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-08-27 11:59:07 +0100

    Add constants file

---
+++ ReadBeeb/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ReadBeeb
+//
+//  Created by Bilaal Rashid on 27/08/2023.
+//
+
+import Foundation
+
+struct Constants {
+
+    static let bundleIdentifier = Bundle.main.bundleIdentifier ?? "uk.co.bilaal.ReadBeeb"
+
+}
