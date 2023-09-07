9d0009b1c463b964bdc41dbd1a9d530f9a2954ec
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-09-07 22:54:51 +0100

    Fetch data for stories

--- ReadBeeb/Views/NewsStoryDetailView.swift
+++ ReadBeeb/Views/NewsStoryDetailView.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+import OSLog
 
 struct NewsStoryDetailView: View {
 
     let destination: BBCNewsAPIFDFluffyDestination
 
+    @State private var data: BBCNewsAPIFDResult? = nil
+
+    @State private var shouldDisplayNetworkError = false
+
     var body: some View {
         Text("Hello, World!")
+            .listStyle(.plain)
+            .alert(
+                "Unable To Load Data",
+                isPresented: self.$shouldDisplayNetworkError,
+                actions: { Button("OK", role: .cancel) { } },
+                message: { Text("Please try again later and contact support if the problem persists") }
+            )
+            .refreshable {
+                Task {
+                    await self.fetchData()
+                }
+            }
+            .onAppear {
+                Task {
+                    await self.fetchData()
+                }
+            }
+    }
+
+    private func fetchData() async {
+        do {
+            let result = try await BBCNewsAPINetworkController.fetchFDUrl(url: self.destination.url)
+            self.data = result
+        } catch let error {
+            self.shouldDisplayNetworkError = true
+            Logger.network.error("Unable to fetch news article \(self.destination.url) - \(error.localizedDescription)")
+        }
     }
     
 }
