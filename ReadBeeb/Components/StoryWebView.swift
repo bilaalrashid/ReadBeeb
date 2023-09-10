303314b0c8110089ffc5e772d053b55e9f6673cc
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-09-11 00:41:29 +0100

    Hide CSS for web view stories

--- ReadBeeb/Components/StoryWebView.swift
+++ ReadBeeb/Components/StoryWebView.swift
@@ -12,13 +12,35 @@ struct StoryWebView: UIViewRepresentable {
 
     let url: URL
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
     func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        return webView
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
         let request = URLRequest(url: url)
         webView.load(request)
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: StoryWebView
+
+        init(_ parent: StoryWebView) {
+            self.parent = parent
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+
+            let css = "#cookiePrompt, .bbccookies-banner, #orbit-header, .orbit-header-container, #u47640082145343365, .nav-top, #orb-footer, .orb-footer { display: none; }"
+            let js = "const style = document.createElement('style'); style.innerHTML = '\(css)'; document.head.appendChild(style);"
+
+            webView.evaluateJavaScript(js)
+        }
     }
 
 }
