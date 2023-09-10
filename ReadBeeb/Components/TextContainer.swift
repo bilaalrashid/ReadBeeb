e31befbcb6f36d8371f9d1cd81728e7641aca6d7
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-09-10 23:46:12 +0100

    Implement story links

--- ReadBeeb/Components/TextContainer.swift
+++ ReadBeeb/Components/TextContainer.swift
@@ -11,18 +11,91 @@ struct TextContainer: View {
 
     let container: FDTextContainer
 
+    @State private var isLinkActive = false
+    @State private var destination: FDLinkDestination? = nil {
+        didSet {
+            self.isLinkActive = true
+        }
+    }
+
     var body: some View {
-        Text(self.container.text.text)
-            .modify {
-                switch self.container.containerType {
-                case "body":
-                    $0.font(.body)
-                case "crosshead":
-                    $0.font(.title3.bold())
-                default:
-                    $0.font(.body)
+        ZStack {
+            Text(.init(self.textFormattedSpans()))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .multilineTextAlignment(.leading)
+                .modify {
+                    switch self.container.containerType {
+                    case "body":
+                        $0.font(.body)
+                    case "crosshead":
+                        $0.font(.title3.bold())
+                    default:
+                        $0.font(.body)
+                    }
                 }
+                .environment(\.openURL, OpenURLAction { url in
+                    if let destination = self.destination(for: url) {
+                        self.destination = destination
+                    }
+
+                    return .handled
+                })
+                .navigationDestination(isPresented: self.$isLinkActive) {
+                    if let destination = self.destination {
+                        StoryDetailView(destination: destination)
+                    }
+                }
+        }
+    }
+
+    private func textFormattedSpans() -> String {
+        let spans = self.container.text.spans.sorted { $0.startIndex < $1.startIndex }
+        let remainingText = self.container.text.text
+
+        var formattedMarkdown = [String]()
+        var lastIndex = 0
+
+        for span in spans {
+            let nextPlainSection = remainingText.substring(from: lastIndex, to: span.startIndex)
+            formattedMarkdown.append(nextPlainSection)
+
+            let extracted = remainingText.substring(from: span.startIndex, to: span.startIndex + span.length)
+
+            if let url = span.link.destinations.first?.url {
+                let markdown = "[\(extracted)](\(url))"
+                formattedMarkdown.append(markdown)
+            } else {
+                formattedMarkdown.append(extracted)
             }
+
+            lastIndex = span.startIndex + span.length
+        }
+
+        let lastPlainSection = remainingText.substring(from: lastIndex, to: remainingText.count)
+        formattedMarkdown.append(lastPlainSection)
+
+        return formattedMarkdown.joined()
+    }
+
+    private func destination(for url: URL) -> FDLinkDestination? {
+        var destinations = [FDLinkDestination]()
+        self.container.text.spans.forEach {
+            destinations.append(contentsOf: $0.link.destinations)
+        }
+
+        return destinations.first {
+            $0.url == url.absoluteString
+        }
+    }
+
+}
+
+extension String {
+
+    func substring(from startIndex: Int, to endIndex: Int) -> String {
+        let start = self.index(self.startIndex, offsetBy: startIndex)
+        let end = self.index(self.startIndex, offsetBy: endIndex)
+        return String(self[start..<end])
     }
 
 }
