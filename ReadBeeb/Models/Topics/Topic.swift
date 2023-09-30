fce3ef6bd0795e139e4967ad856463da1fdb1873
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-09-30 17:03:12 +0100

    Define topic models

---
+++ ReadBeeb/Models/Topics/Topic.swift
@@ -0,0 +1,15 @@
+//
+//  Topic.swift
+//  ReadBeeb
+//
+//  Created by Bilaal Rashid on 30/09/2023.
+//
+
+import Foundation
+import SwiftData
+
+struct Topic {
+    let id: String
+    let headline: String
+    let subhead: String?
+}
---
+++ ReadBeeb/Models/Topics/TopicResult.swift
@@ -0,0 +1,12 @@
+//
+//  TopicResult.swift
+//  ReadBeeb
+//
+//  Created by Bilaal Rashid on 30/09/2023.
+//
+
+import Foundation
+
+struct TopicResult {
+    let topics: [Topic]
+}
