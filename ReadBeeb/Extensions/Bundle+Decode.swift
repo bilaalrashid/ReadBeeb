0c1237f05a8f908044b8d94009b19836d3b72292
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-01 17:03:59 +0100

    Implement topic selection

--- ReadBeeb/Extensions/Bundle+Decode.swift
+++ ReadBeeb/Extensions/Bundle+Decode.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+extension Bundle {
+
+    /// Decode a JSON file for a given type
+    ///
+    /// - Note: This file will through fatalError() if the decoder is unable to decode the JSON to the given type.
+    ///         Files stored in the Bundle are static and hardcoded.
+    ///         It is the responsibility of the developer to ensure that they conform to your desired schema before shipping to production.
+    func decode<T: Decodable>(_ type: T.Type, from file: String, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+        decoder.keyDecodingStrategy = keyDecodingStrategy
+
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' not found – \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+    }
+
+}
--- ReadBeeb/Views/TopicSelectionView.swift
+++ ReadBeeb/Views/TopicSelectionView.swift
@@ -6,11 +6,87 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct TopicSelectionView: View {
+
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+
+    @Query var selectedTopics: [Topic]
+
+    let allTopics: [Topic] = {
+        let result = Bundle.main.decode(TopicResult.self, from: "Topics.json")
+        return result.topics
+    }()
+
     var body: some View {
-        Text("Hello, World!")
+        List {
+            ForEach(self.allTopics, id: \.id) { topic in
+                HStack {
+                    if self.isTopicSelected(topic: topic) {
+                        Image(systemName: "checkmark.square.fill")
+                            // Color.accentColor is not working here
+                            .foregroundStyle(Constants.primaryColor)
+                    } else {
+                        Image(systemName: "square")
+                            // Color.accentColor is not working here
+                            .foregroundStyle(Constants.primaryColor)
+                    }
+
+                    VStack {
+                        Text(topic.headline)
+                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+
+                        if let subhead = topic.subhead {
+                            Text(subhead)
+                                .font(.caption)
+                                .multilineTextAlignment(.leading)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .onTapGesture {
+                    withAnimation {
+                        self.toggleTopicSelection(topic: topic)
+                    }
+                }
+            }
+        }
+        .navigationTitle("My Topics")
+        .toolbarColorScheme(.dark, for: .navigationBar)
+        .toolbarBackground(Constants.primaryColor, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+        .toolbar {
+            Button(action: {
+                self.dismiss()
+            }) {
+                Text("Done")
+                    .font(.headline)
+                    // System tint color overrides the toolbar color scheme, so the color needs explicitly defining
+                    .foregroundStyle(.white)
+            }
+        }
     }
+
+    private func selectedTopic(for topic: Topic) -> Topic? {
+        return self.selectedTopics.first { $0.id == topic.id }
+    }
+
+    private func isTopicSelected(topic: Topic) -> Bool {
+        return self.selectedTopic(for: topic) != nil
+    }
+
+    private func toggleTopicSelection(topic: Topic) {
+        if let existingTopic = self.selectedTopic(for: topic) {
+            self.modelContext.delete(existingTopic)
+        } else {
+            self.modelContext.insert(topic)
+        }
+    }
+
 }
 
 #Preview {
