d09b9e8bb5961fa8774bb7b8debe8840e8724cdb
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-15 23:07:59 +0100

    Show error when fetching empty data for Top Stories tab

--- ReadBeeb/Views/TopStoriesView.swift
+++ ReadBeeb/Views/TopStoriesView.swift
@@ -79,8 +79,14 @@ struct TopStoriesView: View {
         do {
             self.networkRequest = .loading
             let result = try await BBCNewsAPINetworkController.fetchDiscoveryPage()
-            self.structuredItems = result.data.structuredItems.excluding(headers: self.sectionsToExclude)
-            self.networkRequest = .success
+            let structuredItems = result.data.structuredItems.excluding(headers: self.sectionsToExclude)
+            if !structuredItems.isEmpty {
+                self.structuredItems = structuredItems
+                self.networkRequest = .success
+            } else {
+                self.networkRequest = .error
+                Logger.network.error("BBC News API Home tab is empty")
+            }
         } catch let error {
             self.networkRequest = .error
             Logger.network.error("Unable to fetch BBC News API Home tab - \(error.localizedDescription)")
