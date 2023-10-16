3e06acc526c134716df7cdc5cff292ed6ac4b8e3
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-16 22:13:04 +0100

    Fix out of bounds errors

--- ReadBeeb/Extensions/String+Substring.swift
+++ ReadBeeb/Extensions/String+Substring.swift
@@ -10,8 +10,15 @@ import Foundation
 extension String {
 
     func substring(from startIndex: Int, to endIndex: Int) -> String {
+        // Prevent accessing an index out of bounds
+        var safeEndIndex = endIndex
+        if endIndex >= self.count {
+            safeEndIndex = self.count - 1
+        }
+
         let start = self.index(self.startIndex, offsetBy: startIndex)
-        let end = self.index(self.startIndex, offsetBy: endIndex)
+        let end = self.index(self.startIndex, offsetBy: safeEndIndex)
+
         return String(self[start..<end])
     }
 
