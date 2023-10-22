a7afff006e994c4a7be27df8573a2b00ac9948d8
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-22 16:07:11 +0100

    Treat Copyright section as a special-case section header when filtering

--- ReadBeeb/Extensions/FDData+StructuredItems.swift
+++ ReadBeeb/Extensions/FDData+StructuredItems.swift
@@ -37,8 +37,13 @@ extension Array<FDStructuredDataItem> {
     /// Filters out any sections to exclude any that do not match the specified headers
     /// - Parameter includableHeaders: The headers of sections that will not be filtered out
     /// - Returns: The filtered items
+    /// - Note: `"Copyright"`is treated as a special-case section header
     func including(headers includableHeaders: [String]) -> [FDStructuredDataItem] {
         return self.filter {
+            if case .copyright = $0.body {
+                return includableHeaders.contains("Copyright")
+            }
+
             guard let header = $0.header else { return false }
             guard case .collectionHeader(let collectionHeader) = header else { return false }
             return includableHeaders.contains(collectionHeader.text)
@@ -48,8 +53,13 @@ extension Array<FDStructuredDataItem> {
     /// Filters out any sections to exclude any that match the specified headers
     /// - Parameter excludableHeaders: The headers of sections to filter out
     /// - Returns: The filtered items
+    /// - Note: `"Copyright"`is treated as a special-case section header
     func excluding(headers excludableHeaders: [String]) -> [FDStructuredDataItem] {
         return self.filter {
+            if case .copyright = $0.body {
+                return !excludableHeaders.contains("Copyright")
+            }
+
             guard let header = $0.header else { return true }
             guard case .collectionHeader(let collectionHeader) = header else { return true }
             return !excludableHeaders.contains(collectionHeader.text)
