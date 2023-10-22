5528cadf33bce85937141d9035cc37383ba82860
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-23 00:21:49 +0100

    Add note about not using lazy loading for carousel

--- ReadBeeb/Components/Carousel.swift
+++ ReadBeeb/Components/Carousel.swift
@@ -13,6 +13,7 @@ struct Carousel: View {
 
     var body: some View {
         ScrollView(.horizontal) {
+            // Don't use LazyHStack, the layout doesn't render properly if lazy loaded
             HStack(spacing: 16) {
                 ForEach(Array(self.item.items.enumerated()), id: \.offset) { index, storyPromo in
                     VideoPortraitStory(storyPromo: storyPromo)
