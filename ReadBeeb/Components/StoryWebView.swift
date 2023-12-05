d8b5f36b8254e264a5cb69f553b9e8783f42d44c
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-12-05 22:04:35 +0000

    Fix #27

--- ReadBeeb/Components/StoryWebView.swift
+++ ReadBeeb/Components/StoryWebView.swift
@@ -19,12 +19,15 @@ struct StoryWebView: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         webView.navigationDelegate = context.coordinator
+
+        let request = URLRequest(url: url)
+        webView.load(request)
+
         return webView
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        webView.load(request)
+        // noop
     }
 
     class Coordinator: NSObject, WKNavigationDelegate {
