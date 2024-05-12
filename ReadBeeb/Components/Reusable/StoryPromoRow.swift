ff984bb0667aa3d76b0bbd11e54972b2db9d6dfc
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2024-05-12 17:19:44 +0100

    Don't show time-sensitive badges and time in story promo rows

--- ReadBeeb/Components/Reusable/StoryPromoRow.swift
+++ ReadBeeb/Components/Reusable/StoryPromoRow.swift
@@ -14,6 +14,15 @@ struct StoryPromoRow: View {
     // A secondary destination that the story promo can link to e.g. a topic discovery page.
     @Binding var destination: FDLinkDestination?
 
+    /// Returns the appropriate badge to be displayed in the row, if applicable.
+    private var badgeForDisplay: FDBadge? {
+        if let badge = self.story.badges?.first(where: { $0.type == "BREAKING" }) {
+            return badge
+        }
+
+        return self.story.badges?.first { $0.type == "LIVE" }
+    }
+
     var body: some View {
         HStack {
             ThumbnailImageView(image: self.story.image, badges: self.story.badges, prominent: false)
@@ -32,7 +41,7 @@ struct StoryPromoRow: View {
 
                 Spacer(minLength: 1)
 
-                HStack(alignment: .center) {
+                HStack {
                     if let topic = self.story.topic, let text = topic.text {
                         Button(action: {
                             self.destination = topic.link?.destinations.first
@@ -42,31 +51,168 @@ struct StoryPromoRow: View {
                                 .foregroundColor(.accentColor)
                                 .lineLimit(1)
                                 .truncationMode(.tail)
-                                .frame(maxWidth: .infinity, alignment: .leading)
                         }
                         .buttonStyle(.plain)
                     }
 
-                    if let badges = self.story.badges {
-                        ForEach(Array(badges.enumerated()), id: \.offset) { _, badge in
-                            Text(badge.text ?? "")
-                                .font(.caption.weight(.heavy))
-                                .foregroundColor(.accentColor)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.5)
-                        }
-                    }
+                    Spacer()
 
-                    if let lastUpdated = self.story.updated {
+                    if let badge = self.badgeForDisplay, let text = badge.text {
+                        Text(text)
+                            .font(.caption.weight(.heavy))
+                            .foregroundColor(badge.brand == "SPORT" ? Constants.sportColor : .accentColor)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.5)
+                    } else if let lastUpdated = self.story.updated {
                         Text(lastUpdated.formattedTimestamp)
                             .font(.caption)
                             .lineLimit(1)
                             .minimumScaleFactor(0.5)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
                     }
                 }
             }
             .padding(.all, 4)
         }
     }
+}
+
+#Preview {
+    List {
+        StoryPromoRow(
+            story: FDStoryPromo(
+                style: "SMALL_HORIZONTAL_PROMO_CARD",
+                languageCode: "en-gb",
+                text: "Why you probably missed the Northern Lights on Saturday",
+                link: FDLink(destinations: []),
+                subtext: "The Northern Lights waned early on Saturday night, but picked up again much later.",
+                updated: 1715511413682,
+                topic: FDTopic(text: "UK"),
+                image: FDImage(
+                    source: FDImageSource(
+                        url: "https://ichef.bbci.co.uk/moira/img/android/v3/{width}/cpsprodpb/3dfc/live/fd4d6280-102b-11ef-b521-293a36493444.jpg",
+                        sizingMethod: FDImageSizingMethod(
+                            type: "SPECIFIC_WIDTHS",
+                            widthToken: "{width}",
+                            widths: [128, 1024, 2560]
+                        ),
+                        aspectRatio: 1.7772511848341233
+                    ),
+                    metadata: nil
+                ),
+                uasToken: "urn:bbc:optimo:asset:cglv53402mko"
+            ),
+            destination: .constant(nil)
+        )
+        StoryPromoRow(
+            story: FDStoryPromo(
+                style: "SMALL_HORIZONTAL_PROMO_CARD",
+                languageCode: "en-gb",
+                text: "Why you probably missed the Northern Lights on Saturday",
+                link: FDLink(destinations: []),
+                subtext: "The Northern Lights waned early on Saturday night, but picked up again much later.",
+                updated: 1715511413682,
+                topic: FDTopic(text: "Entertainment & Arts"),
+                image: FDImage(
+                    source: FDImageSource(
+                        url: "https://ichef.bbci.co.uk/moira/img/android/v3/{width}/cpsprodpb/3dfc/live/fd4d6280-102b-11ef-b521-293a36493444.jpg",
+                        sizingMethod: FDImageSizingMethod(
+                            type: "SPECIFIC_WIDTHS",
+                            widthToken: "{width}",
+                            widths: [128, 1024, 2560]
+                        ),
+                        aspectRatio: 1.7772511848341233
+                    ),
+                    metadata: nil
+                ),
+                uasToken: "urn:bbc:optimo:asset:cglv53402mko"
+            ),
+            destination: .constant(nil)
+        )
+        StoryPromoRow(
+            story: FDStoryPromo(
+                style: "SMALL_HORIZONTAL_PROMO_CARD",
+                languageCode: "en-gb",
+                text: "Why you probably missed the Northern Lights on Saturday",
+                link: FDLink(destinations: []),
+                subtext: "The Northern Lights waned early on Saturday night, but picked up again much later.",
+                updated: 1715511413682,
+                topic: FDTopic(text: "Entertainment & Arts"),
+                image: FDImage(
+                    source: FDImageSource(
+                        url: "https://ichef.bbci.co.uk/moira/img/android/v3/{width}/cpsprodpb/3dfc/live/fd4d6280-102b-11ef-b521-293a36493444.jpg",
+                        sizingMethod: FDImageSizingMethod(
+                            type: "SPECIFIC_WIDTHS",
+                            widthToken: "{width}",
+                            widths: [128, 1024, 2560]
+                        ),
+                        aspectRatio: 1.7772511848341233
+                    ),
+                    metadata: nil
+                ),
+                badges: [
+                    FDBadge(type: "BREAKING", brand: "NEWS", text: "BREAKING")
+                ],
+                uasToken: "urn:bbc:optimo:asset:cglv53402mko"
+            ),
+            destination: .constant(nil)
+        )
+        StoryPromoRow(
+            story: FDStoryPromo(
+                style: "SMALL_HORIZONTAL_PROMO_CARD",
+                languageCode: "en-gb",
+                text: "Why you probably missed the Northern Lights on Saturday",
+                link: FDLink(destinations: []),
+                subtext: "The Northern Lights waned early on Saturday night, but picked up again much later.",
+                updated: 1715511413682,
+                topic: FDTopic(text: "Entertainment & Arts"),
+                image: FDImage(
+                    source: FDImageSource(
+                        url: "https://ichef.bbci.co.uk/moira/img/android/v3/{width}/cpsprodpb/3dfc/live/fd4d6280-102b-11ef-b521-293a36493444.jpg",
+                        sizingMethod: FDImageSizingMethod(
+                            type: "SPECIFIC_WIDTHS",
+                            widthToken: "{width}",
+                            widths: [128, 1024, 2560]
+                        ),
+                        aspectRatio: 1.7772511848341233
+                    ),
+                    metadata: nil
+                ),
+                badges: [
+                    FDBadge(type: "LIVE", brand: "NEWS", text: "LIVE")
+                ],
+                uasToken: "urn:bbc:optimo:asset:cglv53402mko"
+            ),
+            destination: .constant(nil)
+        )
+        StoryPromoRow(
+            story: FDStoryPromo(
+                style: "SMALL_HORIZONTAL_PROMO_CARD",
+                languageCode: "en-gb",
+                text: "Why you probably missed the Northern Lights on Saturday",
+                link: FDLink(destinations: []),
+                subtext: "The Northern Lights waned early on Saturday night, but picked up again much later.",
+                updated: 1715511413682,
+                topic: FDTopic(text: "Entertainment & Arts"),
+                image: FDImage(
+                    source: FDImageSource(
+                        url: "https://ichef.bbci.co.uk/moira/img/android/v3/{width}/cpsprodpb/3dfc/live/fd4d6280-102b-11ef-b521-293a36493444.jpg",
+                        sizingMethod: FDImageSizingMethod(
+                            type: "SPECIFIC_WIDTHS",
+                            widthToken: "{width}",
+                            widths: [128, 1024, 2560]
+                        ),
+                        aspectRatio: 1.7772511848341233
+                    ),
+                    metadata: nil
+                ),
+                badges: [
+                    FDBadge(type: "LIVE", brand: "SPORT", text: "LIVE")
+                ],
+                uasToken: "urn:bbc:optimo:asset:cglv53402mko"
+            ),
+            destination: .constant(nil)
+        )
+    }
+    .listStyle(.plain)
+    .accentColor(Constants.primaryColor)
 }
